49a5c274adab9b11b01320c2d6314124b9f332ff
crusty7 <crusty7@mail.ru>
2018-02-01 19:01:55 +0300

    Авторизация и регистрация
    Целиком реализованы экраны и технологии авторизации и регистрации.

--- Login.swift
+++ Login.swift
@@ -10,37 +10,62 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var labelConstraint: NSLayoutConstraint!
+    @IBOutlet weak var warningConstraint: NSLayoutConstraint!
     @IBOutlet weak var loginConstraint: NSLayoutConstraint!
     @IBOutlet weak var passwordConstraint: NSLayoutConstraint!
-    @IBOutlet weak var enterConstraint: NSLayoutConstraint!
     @IBOutlet weak var registerConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         
         super.viewDidLoad()
         let screenHeight = view.frame.height
         labelConstraint.constant = screenHeight/7
-        loginConstraint.constant = screenHeight/7
+        warningConstraint.constant = screenHeight/25
+        loginConstraint.constant = screenHeight/20
         passwordConstraint.constant = screenHeight/20
-        enterConstraint.constant = screenHeight/15
-        registerConstraint.constant = screenHeight/7
+        registerConstraint.constant = screenHeight/5
     }
     
     @IBAction func loginAction(_ sender: Any) {
-        let login = loginTextField.text
-        let password = passwordTextField.text
-        //сделать проверку правильности пароля
-        let myUrl = "http://"
-        //let request = NSMutableURLRequest(url: myUrl.toURL)
-        //request.httpMethod = "POST"
-        var postString = "login=(login)&password=(password)"
-        dismiss(animated: true, completion: nil)
+        let login = loginTextField.text!
+        let password = passwordTextField.text!
+        if login.isEmpty || password.isEmpty {
+            warningLabel.text = "Все поля обязательны"
+            warningLabel.textColor = UIColor.red
+        }
+        else {
+            let postString = "login=\(login)&password=\(password)"
+            let myURL = URL(string: "https://mighty-springs-49939.herokuapp.com/signin?\(postString)")!
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            session.dataTask(with: myURL) { (_, response, error) in
+                DispatchQueue.main.async {
+                    let status = (response as! HTTPURLResponse).statusCode
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                    print("response status: \(status)")
+                    switch status {
+                    case 202:
+                        self.dismiss(animated: true, completion: nil)
+                    case 401:
+                        self.warningLabel.text = "Неверное имя пользователя или пароль"
+                        self.warningLabel.textColor = UIColor.red
+                    default:
+                        print("unknown status code")
+                    }
+                }
+                }.resume()
+            //dismiss(animated: true, completion: nil)
+        }
     }
-    @IBAction func regiserAction(_ sender: Any) {
+    
+    @IBAction func registerAction(_ sender: Any) {
         performSegue(withIdentifier: "register", sender: nil)
     }
+    
     
     
 }
--- Registration.swift
+++ Registration.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class RegistrationViewController: UIViewController {
+var name: String = ""
+var surname: String = ""
+var patronymic: String = ""
+
+class FirstStepRegistrationViewController: UIViewController {
     
-    
-    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var surnameTextField: UITextField!
+    @IBOutlet weak var patronymicTextField: UITextField!
+    @IBOutlet weak var warningConstraint: NSLayoutConstraint!
     @IBOutlet weak var nameConstraint: NSLayoutConstraint!
     @IBOutlet weak var surnameConstraint: NSLayoutConstraint!
     @IBOutlet weak var patronymicConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nextConstraint: NSLayoutConstraint!
+    @IBOutlet weak var warningLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let screenHeight = view.frame.height
+        warningConstraint.constant = screenHeight/30
+        nameConstraint.constant = screenHeight/30
+        surnameConstraint.constant = screenHeight/23
+        patronymicConstraint.constant = screenHeight/23
+        nextConstraint.constant = screenHeight/30
+    }
+    
+    @IBAction func nextAction(_ sender: Any) {
+        name = nameTextField.text!
+        surname = surnameTextField.text!
+        patronymic = patronymicTextField.text!
+        if name.isEmpty || surname.isEmpty {
+            warningLabel.text = "Поля имя и фамилия обязательны"
+            warningLabel.textColor = UIColor.red
+        }
+        else {
+            performSegue(withIdentifier: "next", sender: nil)
+        }
+    }
+}
+
+class LastStepRegistrationViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfrimationTextField: UITextField!
+    @IBOutlet weak var warningConstraint: NSLayoutConstraint!
     @IBOutlet weak var loginConstraint: NSLayoutConstraint!
     @IBOutlet weak var passwordConstraint: NSLayoutConstraint!
     @IBOutlet weak var passwordConfirmationConstraint: NSLayoutConstraint!
@@ -22,12 +63,53 @@ class RegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let screenHeight = view.frame.height
-        nameConstraint.constant = screenHeight/12
-        surnameConstraint.constant = screenHeight/23
-        patronymicConstraint.constant = screenHeight/23
-        loginConstraint.constant = screenHeight/23
+        warningConstraint.constant = screenHeight/30
+        loginConstraint.constant = screenHeight/30
         passwordConstraint.constant = screenHeight/23
         passwordConfirmationConstraint.constant = screenHeight/23
-        registerConstraint.constant = screenHeight/15
+        registerConstraint.constant = screenHeight/30
+    }
+    @IBAction func registerAction(_ sender: Any) {
+        let login = loginTextField.text!
+        let password = passwordTextField.text!
+        let passwordConfirmation = passwordConfrimationTextField.text!
+        if login.isEmpty || password.isEmpty || passwordConfirmation.isEmpty {
+            warningLabel.text = "Все поля обязательны"
+            warningLabel.textColor = UIColor.red
+        }
+        else {
+            if passwordConfirmation != password {
+                warningLabel.text = "Пароли не совпадают"
+                warningLabel.textColor = UIColor.red
+            }
+            else {
+                let myUrl = "https://mighty-springs-49939.herokuapp.com/signup?"
+                let request = NSMutableURLRequest(url: URL(string: myUrl)!)
+                request.httpMethod = "POST"
+                let postString = "name=\(name)&surname=\(surname)&patronymic=\(patronymic)&login=\(login)&password=\(password)"
+                //print(postString)
+                request.httpBody = postString.data(using: String.Encoding.utf8)
+                URLSession.shared.dataTask(with: request as URLRequest) {
+                    (_, response, error) in
+                    DispatchQueue.main.async {
+                        let status = (response as! HTTPURLResponse).statusCode
+                        if let error = error {
+                            print(error.localizedDescription)
+                        }
+                        //print("response status: \(status)")
+                        switch status {
+                        case 201:
+                            self.dismiss(animated: true, completion: nil)
+                        case 409:
+                            self.warningLabel.text = "Такой логин уже существует"
+                            self.warningLabel.textColor = UIColor.red
+                        default:
+                            print("unknown status code")
+                        }
+                    }
+                }.resume()
+                //dismiss(animated: true, completion: nil)
+            }
+        }
     }
 }
